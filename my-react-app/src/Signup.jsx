b661975b12c30b4c93e35c394319854b83fe5de7
Binda <bindalearning@gmail.com>
2024-06-04 20:51:54 +0545

    internal css

---
+++ my-react-app/src/Signup.jsx
@@ -0,0 +1,19 @@
+
+function Signup(){
+
+    const styles = {
+    backgroundColor: "yellow",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+    marginLeft: "5%",
+    }
+
+
+    return(
+        <button style ={styles}> Sign Up </button>
+    );
+}
+
+export default Signup
