588debd40b85fa15b76df0c93826be00e259e663
Binda <bindalearning@gmail.com>
2024-06-05 18:42:16 +0545

    props example

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Button from "./Button";
 import Login from "./Login/Login";
 import Signup from "./Signup";
 import Card from "./card";
+import Propexample from "./Propsexample";
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
      <Button/>
      <Login/>
      <Signup/>
+     <Propexample name ="Binda" project="Learning With Leapfrog" day={5} isActive={true}/>
+     <Propexample name ="Lemon" project="Learn about props" day="50" isActive={false}/>
+     <Propexample />
     </>
   );
 }
---
+++ my-react-app/src/Propsexample.jsx
@@ -0,0 +1,32 @@
+
+// props = read-only properties that are shared between components. A parent component can send data to a child component. <component key =value/>
+// propTypes = a mechanism that ensures that the passed value is of the correct datatype. Good practice for debugging age: PropTypes.number
+//defaultProps = default values for props in case they are not passed from the parent component. name: "Guest"
+
+import PropTypes from 'prop-types'
+
+function Propexample(props){
+    return(
+        <div className='props'>
+            <h1> Name: {props.name}</h1>
+            <h1> Project: {props.project}</h1>
+            <h1> Day: {props.day}</h1>
+            <h1> Active: {props.isActive ? "Yes" : "No"}</h1>
+        </div>
+    );
+}
+
+Propexample.propTypes = {
+    name: PropTypes.string,
+    project: PropTypes.string,
+    day: PropTypes.number,
+    isActive: PropTypes.bool,
+}
+
+Propexample.defaultProps ={
+    name: "Froggy",
+    project: "ReactJs",
+    day: 0,
+    isActive: "No",
+}
+export default Propexample
